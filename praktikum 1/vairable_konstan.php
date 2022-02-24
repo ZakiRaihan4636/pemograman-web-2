90b12d28fe0ccb470e9a4d72f76a5e08c3d8e087
Muhamad Zaki Raihan <toteyoku4636@gmail.com>
2022-02-24 10:18:08 +0700

    Praktikum 1 Array dan Variable

---
+++ praktikum 1/vairable_konstan.php
@@ -0,0 +1,19 @@
+<?php 
+  // Definisikan konstansta
+  define('PHI',3.14);
+  define('DBNAME','inventori');
+  define('DBSERVER','localhost');
+
+  $jadi = 10;
+  $luas = PHI * $jari *$jari;
+  $kel = 2 * PHI *$jari;
+
+  echo 'Luas Lingkaran dengan Jari'.$jari.' : ' .$luas;
+  echo '<br/> kelilingnya : '. $kel; 
+?>
+<hr>
+<?php 
+  echo 'Nama databasenya :'.DBNAME;
+  echo '<br/>Lokasi databasenya ada di'.DBSERVER;
+  
+?>
---
+++ praktikum 1/variable_system.php
@@ -0,0 +1,6 @@
+<?php 
+  // Variable system
+  echo 'Document Root'. $_SERVER["DOCUMENT_ROOT"];
+  echo '<br/> Nama file'. $_SERVER["PHP_SELF"];
+
+?>

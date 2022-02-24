66b8d245e0bbf66a79471f7aec94f4c4883594b7
Muhamad Zaki Raihan <toteyoku4636@gmail.com>
2022-02-24 10:05:56 +0700

    praktikum 1 array dan variable

---
+++ praktikum 1/array_asosiatif.php
@@ -0,0 +1,13 @@
+<?php 
+
+$mahasiswa = ["nama" => "Muhamad Zaki Raihan", "kelas" => "TI04", "jurusan" => "Teknik Informatika"];
+
+echo 'Nama  :  '.$mahasiswa['nama'];
+echo  '<br>';
+echo 'Kelas  :  '.$mahasiswa['kelas'];
+echo  '<br>';
+echo 'Jurusan  :  '.$mahasiswa['jurusan'];
+
+
+
+?>
